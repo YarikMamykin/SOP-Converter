b5b39b8a2ac626e71113af7c7e8f08396a94c075
YarikM <ymamykin@gmail.com>
2018-06-16 08:50:38 +0300

    files uploader script added

---
+++ upload_files.bat
@@ -0,0 +1,7 @@
+@echo off
+set target_ip=yarik@192.168.0.%1
+set workspace=/home/yarik/Documents/diploma
+set password=yarik
+set file=%2
+
+pscp -r -pw %password% ./%file% %target_ip%:%workspace%/%file% 
